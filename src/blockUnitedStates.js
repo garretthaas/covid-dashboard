36baac381f9f25ebc1ce276ebdb03a26a6be78c4
Garrett <garretthaas@gmail.com>
2020-07-03 18:30:06 -0700

    added US line graph

--- src/blockUnitedStates.js
+++ src/blockUnitedStates.js
@@ -4,9 +4,8 @@ import { visLineChart } from "./visLineChart";
 
 export default function blockUnitedStates() {
     // cumulative cases
-    console.log("HEY GARRY" + getDataNational.totalCases);
-    visLineChart("https://coviddata.github.io/coviddata/v1/countries/stats.json", "united-states", "cumulative", "cases");
+    visLineChart("https://covidtracking.com/api/v1/us/daily.json", "cumulative", "cases");
     
     // cumulative deaths
-    visLineChart("https://coviddata.github.io/coviddata/v1/countries/stats.json", "united-states", "cumulative", "deaths");
+    visLineChart("https://covidtracking.com/api/v1/us/daily.json", "cumulative", "deaths");
 };
