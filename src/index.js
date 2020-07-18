e5df5e8d6c7086b05381a8c3084a3a8fa700e931
h0plyn <ricky@rickyrhodes.com>
2020-07-17 21:05:22 -0400

    font styling for devices

--- src/index.js
+++ src/index.js
@@ -9,10 +9,11 @@ blockCounty();
 
 module;
 function component() {
-    const element = document.createElement('div');
+    const element = document.createElement('p');
+    element.classList.add('callout')
   
     // Lodash, now imported by this script
-    element.innerHTML = _.join(['Made with 😷 Los Angeles, CA & Cleveland, OH'], ' ');
+    element.innerHTML = _.join([' '], ' ');
   
     return element;
   } 
