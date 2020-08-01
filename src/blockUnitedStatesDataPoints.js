78a125de34169de950fb436f3f7b39eb2a751e3e
h0plyn <ricky@rickyrhodes.com>
2020-07-31 21:10:13 -0400

    fixing % change bug

--- src/blockUnitedStatesDataPoints.js
+++ src/blockUnitedStatesDataPoints.js
@@ -3,7 +3,6 @@ const getDataNational = () => {
     fetch('https://covidtracking.com/api/v1/us/daily.json')
     .then(response => response.json())
     .then(result => {
-
         //Storing calculations in variables and handling rolling averages
         //Total Cases
         const totalCases = result[0].positive;
@@ -27,7 +26,7 @@ const getDataNational = () => {
 
 
         //Positive Percentage of Total Tests
-        const totalPositive = Math.round(((result[0].positive / result[0].negative) * 100));
+        const totalPositive = ((result[0].positive / result[0].negative) * 100).toFixed(1);
 
         const positiveChangeRollingAverage = () => {
           let positiveDailyChange = [];
@@ -83,10 +82,8 @@ const getDataNational = () => {
         
         //REFACTOR NAMING CONVENTION
         //Total Cases 7-day average
-       if (Math.sign(totalChangeRollingAverage()) === 1) {
         let prevDayNeg = parent.querySelector('[data-point="cumulative-percent-change"] .detail');
 
-        //@GH — can we use .toggle here?
           if (prevDayNeg.classList.contains('negative')) {
             prevDayNeg.classList.remove('negative');
             prevDayNeg.classList.add('positive');
@@ -94,22 +91,23 @@ const getDataNational = () => {
           } else {
             let printTotalChange = parent.querySelector('[data-point="cumulative-percent-change"] [data-item="data"]');
             printTotalChange.innerHTML = totalChangeRollingAverage()  + '%';
-          }
-      };
+          };
+    
 
         //Positive Percentage of Total Cases
         let printPositiveChange = parent.querySelector('[data-point="positive-tests"] [data-item="content"]');
         printPositiveChange.innerHTML = totalPositive + '%';
-        console.log(totalPositive);
 
         //Positive Percentage 7-day average
-        if (Math.sign(positiveChangeRollingAverage()) === 1) {
+
+
           //Scope into printing location 
           let printPrevDayPos = parent.querySelector('[data-point="positive-percent-change"] [data-item="data"]');
           
           //Scope into negative class
           let changeNegPositive = parent.querySelector('[data-point="positive-percent-change"] .detail');
-           
+           console.log(changeNegPositive)
+           console.log(printPrevDayPos)
           //If negative, statement runs changing .negative to .positive
           if (changeNegPositive.classList.contains('negative')) {
             changeNegPositive.classList.remove('negative');
@@ -118,8 +116,10 @@ const getDataNational = () => {
             } else {
               //if class is positive, print function to browser
               printPrevDayPos.innerHTML = positiveChangeRollingAverage() + '%';
-            }
-        };
+            };
+
+
+
 
 
         //Total Deaths
@@ -127,7 +127,7 @@ const getDataNational = () => {
         printTotalDeaths.innerHTML = totalDeath.toLocaleString()
 
         //Total Deaths 7-day average
-        if (Math.sign(deathChangeRollingAverage()) === 1) {
+
           let changePosDeaths = parent.querySelector('[data-point="deaths-percent-change"] .detail')
           //@GH — can we use .toggle here?
             if (changePosDeaths.classList.contains('negative')) {
@@ -136,30 +136,28 @@ const getDataNational = () => {
               changePosDeaths.innerHTML = deathChangeRollingAverage() + '%'
             } else {
               changePosDeaths.innerHTML = deathChangeRollingAverage() + '%'
-            }
-        };
+            };
+
         
 
         //Current Hospitalizations
         let printCurrentHospitalizations = parent.querySelector('[data-point="hospitalizations"] [data-item="content"]');
         printCurrentHospitalizations.innerHTML = totalHospitalizedCurrent.toLocaleString();
-        
-        //Conditional statement determines if +/-
-        if (Math.sign(hopitalizedChangeRollingAverage()) === 1) {
-          //scope into negative class
-          let changeNegHospitalized = parent.querySelector('[data-point="hospitalized-percent-change"] .detail');
-          //if negative, statement runs changing .negative to .positive
-          if (changeNegHospitalized.classList.contains('negative')) {
-                changeNegHospitalized.classList.remove('negative');
-                changeNegHospitalized.classList.add('positive');
-                changeNegHospitalized.innerHTML = hopitalizedChangeRollingAverage() + '%';
+
+
+          let changeNegHospitalized = parent.querySelector('[data-point="hospitalized-percent-change"] .detail')
+          let printHospitalizationChange = parent.querySelector('[data-point="hospitalized-percent-change"] [data-item="data"]');
+          //@GH — can we use .toggle here?
+            if (changeNegHospitalized.classList.contains('negative')) {
+              changeNegHospitalized.classList.remove('negative')
+              changeNegHospitalized.classList.add('positive')
+              printHospitalizationChange.innerHTML = hopitalizedChangeRollingAverage() + '%'
+
             } else {
-              //if class is positive, print function to browser
-              //scope into printing location 
-              let printHospitalizationChange = parent.querySelector('[data-point="hospitalized-percent-change"] [data-item="data"]');
-              printHospitalizationChange.innerHTML = hopitalizedChangeRollingAverage() + '%';
-            }
-        };
+              printHospitalizationChange.innerHTML = hopitalizedChangeRollingAverage() + '%'
+              console.log(hopitalizedChangeRollingAverage())
+
+            };
         
     })
     .catch(error => console.log('error', error));
